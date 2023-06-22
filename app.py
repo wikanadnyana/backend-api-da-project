78f0c43005ea64ce97fa7f35e9e9e9908db5e9a5
wikanadnyana <wikanadnyana44@gmail.com>
2023-06-22 17:08:53 +0800

    [FIX] finish sending response to console

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from pymongo.server_api import ServerApi
 import pickle
 import numpy as np
 from datetime import datetime
+from flask_cors import cross_origin
+
 
 app = Flask(__name__)
 
@@ -18,25 +20,34 @@ database = client['football-predict']
 collection = database['master']
 
 @app.route("/predict", methods=['POST'])
+@cross_origin()
 def predict():
     data = request.get_json()
+    data["home_code"] = int(data["home_code"])
+    # data.headers.add("Access-Control-Allow-Origin", "*")
     latest_data = collection.find_one({'home_code': data['home_code']}, sort=[('date', -1)])
     tanggal_waktu = data["date"]
     dt = datetime.strptime(tanggal_waktu, "%Y-%m-%d")
     day = dt.day
-
-    data_input = [latest_data['round'], latest_data['venue'], latest_data['gf'] , latest_data['ga'], latest_data['xg'], latest_data['xga'], latest_data['poss'],
-                   latest_data['formation'], latest_data['sh'], latest_data['sot'], latest_data['dist'], latest_data['fk'], latest_data['pk'], latest_data['pkatt']
-                   , latest_data['venue_code'], data['opp_code'], data['hour'], day, 
-                   latest_data['gf_rolling'], latest_data['ga_rolling'],
-                   latest_data['sh_rolling'], latest_data['sot_rolling'], latest_data['dist_rolling'], latest_data['fk_rolling'],
-                    latest_data['pk_rolling'], latest_data['pkatt_rolling'], data['home_code']]
+    # if latest_data is not None:
+    data_input = [
+            int(latest_data['round']), float(latest_data['venue']), float(latest_data['gf']), float(latest_data['ga']),
+            float(latest_data['xg']), float(latest_data['xga']), float(latest_data['poss']),
+            float(latest_data['formation']), float(latest_data['sh']), float(latest_data['sot']),
+            float(latest_data['dist']), float(latest_data['fk']), float(latest_data['pk']), float(latest_data['pkatt']),
+            float(latest_data['venue_code']), int(data['opp_code']), int(data['hour']), day,
+            float(latest_data['gf_rolling']), float(latest_data['ga_rolling']), float(latest_data['sh_rolling']),
+            float(latest_data['sot_rolling']), float(latest_data['dist_rolling']), float(latest_data['fk_rolling']),
+            float(latest_data['pk_rolling']), float(latest_data['pkatt_rolling']), int(data['home_code'])
+        ]
     data_input = np.array(data_input)
     data_input = data_input.reshape(1, -1)
     data_input= data_input.reshape((1, 1, 27))
     prediction = model.predict(data_input)
     prediction = float(prediction)
     away_prediction = 100 - prediction
+    # else :
+        # return jsonify({"Message": type(data["home_code"])})
 
     if data["opp_code"] == data["home_code"] :
         return jsonify({"Message" : "The Team Cannot Same"})
